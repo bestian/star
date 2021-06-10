711622cba9948b2b9611577b3556cd4d8004c7e4
Bestian Tang <bestian@gmail.com>
2021-06-10 16:12:23 +0800

    small

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,21 +7,30 @@
     <br/>
     <svg height="400" width="400">
       <g v-for = "(p,idx) in paths" :key="idx">
-        <line :x1="p.x1" :y1="p.y1" :x2="p.x2" :y2="p.y2" style="stroke:rgb(255,0,0);stroke-width:2" />
+        <line :x1="p.x1" :y1="p.y1" :x2="p.x2" :y2="p.y2" :style="{stroke: color, 'stroke-width':2}" />
       </g>
       <circle v-for = "j in items" :key="j * 100 + 100" :cx="100* Math.cos(3.14 * 2 * j / items.length) + 200" :cy="100 * Math.sin(3.14 * 2 * j / items.length) + 200" r="4" stroke="black" stroke-width="3" fill="red" />
     </svg>
+
+    <color-picker v-model="color"></color-picker>
   </div>
 </template>
 
 <script>
+
+import ColorPicker from 'vue-color-picker-wheel';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
+  components: {
+      ColorPicker
+  },
   data() {
     return {
+      color: '#0f0',
       n: 10,
       d: 3,
       items: [0,1,2,3,4,5,6,7,8,9],
