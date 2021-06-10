a1c695b06821290469d371f61ae473901f0da405
Bestian Tang <bestian@gmail.com>
2021-06-10 10:54:03 +0800

    v1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    點數<input type="number" name="" v-model="n"/>
+    <br/>
+    間隔<input type="number" name="" v-model="d"/>
+    <br/>
     <svg height="400" width="400">
-      <circle v-for = "i in [0,1,2,3,4,5,6,7,8,9]" :key="i" :cx="100* Math.cos(3.14 * i / 5) + 200" :cy="100 * Math.sin(3.14 * i / 5) + 200" r="4" stroke="black" stroke-width="3" fill="red" />
+      <g v-for = "(p,idx) in paths" :key="idx">
+        <line :x1="p.x1" :y1="p.y1" :x2="p.x2" :y2="p.y2" style="stroke:rgb(255,0,0);stroke-width:2" />
+      </g>
+      <circle v-for = "j in items" :key="j * 100 + 100" :cx="100* Math.cos(3.14 * 2 * j / items.length) + 200" :cy="100 * Math.sin(3.14 * 2 * j / items.length) + 200" r="4" stroke="black" stroke-width="3" fill="red" />
     </svg>
   </div>
 </template>
@@ -12,6 +19,47 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      n: 10,
+      d: 3,
+      items: [0,1,2,3,4,5,6,7,8,9],
+      paths: null
+    }
+  },
+  methods: {
+    reset() {
+      let list = [];
+      let pist = [];
+      const l = this.n;
+      for (var i = 0; i < this.n; i++) {
+        list.push(i)
+        let obj = {}
+        obj.x1 = 100 * Math.cos(3.14 * 2 * i * this.d / l) + 200
+        obj.y1 = 100 * Math.sin(3.14 * 2 * i * this.d / l) + 200
+        obj.x2 = 100 * Math.cos(3.14 * 2 * (i+1) * this.d / l) + 200
+        obj.y2 = 100 * Math.sin(3.14 * 2 * (i+1) * this.d / l) + 200
+        pist.push(obj)
+        console.log(i);
+        console.log(obj);
+      }
+      this.items = list;
+      this.paths = pist;
+      console.log(pist);
+      this.$forceUpdate;
+    },
+  },
+  watch: {
+    n(){
+      this.reset()
+    },
+    d() {
+      this.reset()
+    }
+  },
+  mounted() {
+    this.reset()
   }
 }
 </script>
