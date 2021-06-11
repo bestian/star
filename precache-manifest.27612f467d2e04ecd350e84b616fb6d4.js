c7ae7e224edd5cd1368433cef015e549148c96ea
Bestian Tang <bestian@gmail.com>
2021-06-11 09:53:27 +0800

    deploy

---
+++ precache-manifest.27612f467d2e04ecd350e84b616fb6d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdc91a209bea2876d99",
+    "url": "/star/css/app.710f03f2.css"
+  },
+  {
+    "revision": "f2da039f50e504da2f4e50407078161d",
+    "url": "/star/index.html"
+  },
+  {
+    "revision": "ec4d81ebac7620da7042",
+    "url": "/star/js/about.1a52a34d.js"
+  },
+  {
+    "revision": "1fdc91a209bea2876d99",
+    "url": "/star/js/app.0a5cc1eb.js"
+  },
+  {
+    "revision": "6e1016e7df10b0fcf63e",
+    "url": "/star/js/chunk-vendors.f1b5f9d2.js"
+  },
+  {
+    "revision": "44758fcf732d0152da96fb8cb6acac3b",
+    "url": "/star/logo.png"
+  },
+  {
+    "revision": "0597626470e8d720ec6b617363eb5b64",
+    "url": "/star/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/star/robots.txt"
+  }
+]);
